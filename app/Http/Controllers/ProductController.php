9006e0ff51f4cb2edadc0c82d8e560b3e056eb18
Nashinotane <74orisssa84@gmail.com>
2024-01-14 01:50:13 +0900

    stoteアクション

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -37,7 +37,13 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->save();
+
+        return to_route('products.index');
     }
 
     /**
