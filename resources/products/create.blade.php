b33682b600fa435e637aac8a004e8c0c7e88cbc2
Nashinotane <74orisssa84@gmail.com>
2024-01-14 02:52:41 +0900

    修正

--- resources/products/create.blade.php
+++ resources/products/create.blade.php
@@ -1,25 +1,25 @@
-+<div>
-+    <h2>Add New Product</h2>
-+</div>
-+<div>
-+    <a href="{{ route('products.index') }}"> Back</a>
-+</div>
-+
-+<form action="{{ route('products.store') }}" method="POST">
-+    @csrf
-+
-+    <div>
-+        <strong>Name:</strong>
-+        <input type="text" name="name" placeholder="Name">
-+    </div>
-+    <div>
-+        <strong>Description:</strong>
-+        <textarea style="height:150px" name="description" placeholder="Description"></textarea>
-+    </div>
-+    <div>
-+        <strong>Price:</strong>
-+        <input type="number" name="price" placeholder="Price">
-+    </div>
+<div>
+    <h2>Add New Product</h2>
+</div>
+<div>
+    <a href="{{ route('products.index') }}"> Back</a>
+</div>
+
+<form action="{{ route('products.store') }}" method="POST">
+    @csrf
+
+    <div>
+        <strong>Name:</strong>
+        <input type="text" name="name" placeholder="Name">
+    </div>
+    <div>
+        <strong>Description:</strong>
+        <textarea style="height:150px" name="description" placeholder="Description"></textarea>
+    </div>
+    <div>
+        <strong>Price:</strong>
+        <input type="number" name="price" placeholder="Price">
+    </div>
      <div>
          <strong>Category:</strong>
          <select name="category_id">
@@ -28,8 +28,8 @@
          @endforeach
          </select>
      </div>
-+    <div>
-+        <button type="submit">Submit</button>
-+    </div>
-+
-+</form>
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+
+</form>
