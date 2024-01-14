9c2e702e7aef7f59b8c129fca9b8f007c9679d6b
Nashinotane <74orisssa84@gmail.com>
2024-01-14 11:48:36 +0900

    修正

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Gloudemans\Shoppingcart\Facades\Cart;
+ use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -13,7 +15,15 @@ class CartController extends Controller
      */
     public function index()
     {
-        //
+        $cart = Cart::instance(Auth::user()->id)->content();
+ 
+         $total = 0;
+ 
+         foreach ($cart as $c) {
+             $total += $c->qty * $c->price;
+         }
+ 
+         return view('carts.index', compact('cart', 'total'));
     }
 
    
@@ -25,9 +35,19 @@ class CartController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Cart::instance(Auth::user()->id)->add(
+            [
+                'id' => $request->id, 
+                'name' => $request->name, 
+                'qty' => $request->qty, 
+                'price' => $request->price, 
+                'weight' => $request->weight, 
+            ] 
+        );
+
+        return to_route('products.show', $request->get('id'));
     }
-    
+
 
     /**
      * Remove the specified resource from storage.
