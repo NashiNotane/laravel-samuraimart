92e904a6040f5cf439c4c2bf4b8ed3e4c7afa4f5
Nashinotane <74orisssa84@gmail.com>
2024-01-14 09:00:47 +0900

    ビュー編集

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -61,6 +61,7 @@
          <div class="row">
                  @foreach($reviews as $review)
                  <div class="offset-md-5 col-md-5">
+                     <p class="h3">{{$review->title}}</p>
                      <p class="h3">{{$review->content}}</p>
                      <label>{{$review->created_at}} {{$review->user->name}}</label>
                  </div>
@@ -72,6 +73,11 @@
                  <div class="offset-md-5 col-md-5">
                      <form method="POST" action="{{ route('reviews.store') }}">
                          @csrf
+                         <h4>タイトル</h4>
+                         @error('title')
+                             <strong>タイトルを入力してください</strong>
+                         @enderror
+                         <input type="text" name="title" class="form-control m-2">
                          <h4>レビュー内容</h4>
                          @error('content')
                              <strong>レビュー内容を入力してください</strong>
